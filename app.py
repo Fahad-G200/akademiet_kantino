3122e39e9a50e2250d42b05ac028552dd1411dc1
Fahad-G200 <fah_08@icloud.com>
2025-10-20 14:56:42 +0200

    Lager rute for forsiden slik at brukeren ser startsiden først (index.html)

--- app.py
+++ app.py
@@ -2,3 +2,8 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
+# Lager en rute for forsiden slik at brukeren ser satrtsiden først 
+@app.route('/') 
+def index():
+    return render_template('index.html') # Viser startsiden (index.html)
