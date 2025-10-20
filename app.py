d6f5ad7c9af888f8951685749ffb6218c1fa63cd
Fahad-G200 <fah_08@icloud.com>
2025-10-20 14:57:19 +0200

    Laget ukens meny som viser en liste med retter for hver dag

--- app.py
+++ app.py
@@ -7,3 +7,20 @@ app = Flask(__name__)
 @app.route('/') 
 def index():
     return render_template('index.html') # Viser startsiden (index.html)
+
+
+
+# Ukens meny
+# Viser en liste med retter for hver dag
+
+@app.route("/meny")
+def meny():
+    ukens_meny = [
+        "Mandag: Pizza",
+        "Tirsdag: Taco",
+        "Onsdag: Pasta",
+        "Torsdag: Kylling",
+        "Fredag: Pizza"
+    ]
+    return render_template("meny.html", ukens_meny=ukens_meny)
+
